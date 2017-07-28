724845325643c32e304b687364f29fcde63832e0
BBMAG\stuestde <stefan.stuetzer@bbraun.com>
2017-07-28 16:06:54 +0200

    work in progress: sample test /product with assertion on 2 fields

--- src/test/java/com/bbraun/hybris/occ/OCCProductRequestTest.java
+++ src/test/java/com/bbraun/hybris/occ/OCCProductRequestTest.java
@@ -2,8 +2,8 @@ package com.bbraun.hybris.occ;
 
 import org.junit.Test;
 
-import com.bbraun.bbmtest.ws.BBMOAuth2WebServiceTestBuilder;
-import com.bbraun.bbmtest.ws.BBMOAuth2WebServiceTestBuilder.GrantType;
+import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder;
+import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder.GrantType;
 
 import io.restassured.http.ContentType;
 
@@ -11,27 +11,29 @@ import io.restassured.http.ContentType;
  * Tests of Hybris OCC {@code /products} webservice operation.
  * 
  * @author stuestde
- *
  */
 public class OCCProductRequestTest {
 
 	@Test
-	public void testOCCProductsAEMUser() {
-		new BBMOAuth2WebServiceTestBuilder() //
+	public void testOCCProductOperationWithAEMUserEn01() {
+		new BBMWebServiceTestBuilder<>() //
 				.withNewRequest() //
 				.withBasicAuth("trusted_client", "secret") //
 				.doAuthenticateOAuth2("https://qas-ws.hybris.bbraun.com/authorizationserver/oauth/token", //
-						GrantType.PASSWORD, "bbraunoccs", "bbraunoccs", "accessToken")
-
+						GrantType.PASSWORD, //
+						"bbraunoccs", //
+						"bbraunoccs", //
+						"accessToken")
 				.withNewRequest() //
 				.withQueryParam("applicationKey", "AEM2015")//
-				.withQueryParam("viewId", "en_01")
-				.withQueryParam("access_token", "cc85ec80-20b1-4217-8ed4-980255bdd81a") //
-				.doGet("https://qas-ws.hybris.bbraun.com/bbraunocc/v2/bbraun/products/PRID00001252") //
-				
-				.assertResponseContentType(ContentType.JSON)
+				.withQueryParam("viewId", "en_01") //
+				.withQueryParam("access_token", "${accessToken}") //
 
+				.doGet("https://qas-ws.hybris.bbraun.com/bbraunocc/v2/bbraun/products/PRID00001270") //
+
+				.assertResponseContentType(ContentType.JSON) //
+				.assertResponseByPathEquals("url", "/p/PRID00001270") //
+				.assertResponseByPathEquals("code", "PRID00001270") //
 		;
 	}
-
 }
