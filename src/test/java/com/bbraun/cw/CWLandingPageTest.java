b4f36096d9ca0ff34829a34fc0f9f1b24253587e
BBMAG\stuestde <stefan.stuetzer@bbraun.com>
2017-08-03 11:40:39 +0200

    sample test CW

---
+++ src/test/java/com/bbraun/cw/CWLandingPageTest.java
@@ -0,0 +1,23 @@
+package com.bbraun.cw;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import com.bbraun.bbmtest.ui.BBMUiTestBuilder;
+
+public class CWLandingPageTest {
+
+	@Test
+	public void testSearchProductCW() {
+		new BBMUiTestBuilder<>() //
+		.doStartBrowser() //
+		.doOpenUrl("https://www.bbraun.de/de.html") //
+		.doType(By.name("q"), "Vasofix")
+		.doSubmitForm(By.className("_search")) //
+		.assertTextDisplayedOnPage("25 Suchergebnisse") //
+		
+		.doCloseBrowser() //
+		;
+	}
+	
+}
