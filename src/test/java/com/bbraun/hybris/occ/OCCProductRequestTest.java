5625dfd8364fc9178a6b15810791af81761212cf
BBMAG\stuestde <stefan.stuetzer@bbraun.com>
2017-08-09 11:36:09 +0200

    test /product Product with modifiedSince request

--- src/test/java/com/bbraun/hybris/occ/OCCProductRequestTest.java
+++ src/test/java/com/bbraun/hybris/occ/OCCProductRequestTest.java
@@ -38,44 +38,70 @@ public class OCCProductRequestTest {
 	}
 
 	@Test
-	public void testOCCProductRequestProductRelatedBPRWithAEMUserEn01() {
+	public void testOCCProductRequestWithModifiedSinceFlag() {
+		long unixTimestampNow = System.currentTimeMillis() / 1000;
+		long unixTimestamp5MinutesAgo = unixTimestampNow - (5 * 60);
+
 		new BBMWebServiceTestBuilder<>() //
-		.withNewRequest() //
-		.withBasicAuth("trusted_client", "secret") //
-		.doAuthenticateOAuth2("${hybris.oauth.url}", //
-				GrantType.PASSWORD, //
-				"${hybris.oauth.username}", //
-				"${hybris.oauth.password}") //
-		.withNewRequest() //
-		.withQueryParam("applicationKey", "AEM2015")//
-		.withQueryParam("viewId", "en_01") //
-		.withQueryParam("access_token", "${accessToken}") //
-		
-		.doGet("${hybris.BPR000000000000000100007757500000.url}") //
-		
-		.assertResponseContentType(ContentType.JSON) //
-		.assertResponseBodyEqualsReference("OCC_BPR000000000000000100007757500000_Expected.json") //
+				.withNewRequest() //
+				.withBasicAuth("trusted_client", "secret") //
+				.doAuthenticateOAuth2("${hybris.oauth.url}", //
+						GrantType.PASSWORD, //
+						"${hybris.oauth.username}", //
+						"${hybris.oauth.password}") //
+				.withNewRequest() //
+				.withQueryParam("applicationKey", "AEM2015")//
+				.withQueryParam("viewId", "en_01") //
+				.withQueryParam("access_token", "${accessToken}") //
+				.withQueryParam("modifiedSince", unixTimestamp5MinutesAgo) //
+
+				.doGet("${hybris.prid1270.url}") //
+
+				.assertResponseContentType(ContentType.JSON) //
+				.assertResponseBodyByPathEquals("resourceStatus", "NOT_MODIFIED") //
+				.assertResponseBodyByPathEquals("code", "PRID00001270") //
 		;
 	}
-	
+
+	@Test
+	public void testOCCProductRequestProductRelatedBPRWithAEMUserEn01() {
+		new BBMWebServiceTestBuilder<>() //
+				.withNewRequest() //
+				.withBasicAuth("trusted_client", "secret") //
+				.doAuthenticateOAuth2("${hybris.oauth.url}", //
+						GrantType.PASSWORD, //
+						"${hybris.oauth.username}", //
+						"${hybris.oauth.password}") //
+				.withNewRequest() //
+				.withQueryParam("applicationKey", "AEM2015")//
+				.withQueryParam("viewId", "en_01") //
+				.withQueryParam("access_token", "${accessToken}") //
+
+				.doGet("${hybris.BPR000000000000000100007757500000.url}") //
+
+				.assertResponseContentType(ContentType.JSON) //
+				.assertResponseBodyEqualsReference("OCC_BPR000000000000000100007757500000_Expected.json") //
+		;
+	}
+
 	@Test
 	public void testOCCProductRequestImageBPGWithAEMUserEn01() {
 		new BBMWebServiceTestBuilder<>() //
-		.withNewRequest() //
-		.withBasicAuth("trusted_client", "secret") //
-		.doAuthenticateOAuth2("${hybris.oauth.url}", //
-				GrantType.PASSWORD, //
-				"${hybris.oauth.username}", //
-				"${hybris.oauth.password}") //
-		.withNewRequest() //
-		.withQueryParam("applicationKey", "AEM2015")//
-		.withQueryParam("viewId", "en_01") //
-		.withQueryParam("access_token", "${accessToken}") //
-		
-		.doGet("${hybris.BPG000000000000000100006550700000.url}") //
-		
-		.assertResponseContentType(ContentType.JSON) //
-		.assertResponseBodyEqualsReference("OCC_BPG000000000000000100006550700000_Expected.json") //
+				.withNewRequest() //
+				.withBasicAuth("trusted_client", "secret") //
+				.doAuthenticateOAuth2("${hybris.oauth.url}", //
+						GrantType.PASSWORD, //
+						"${hybris.oauth.username}", //
+						"${hybris.oauth.password}") //
+				.withNewRequest() //
+				.withQueryParam("applicationKey", "AEM2015")//
+				.withQueryParam("viewId", "en_01") //
+				.withQueryParam("access_token", "${accessToken}") //
+
+				.doGet("${hybris.BPG000000000000000100006550700000.url}") //
+
+				.assertResponseContentType(ContentType.JSON) //
+				.assertResponseBodyEqualsReference("OCC_BPG000000000000000100006550700000_Expected.json") //
 		;
 	}
 }
