c045094ed454afbe94fac310b82c09933cff580b
BBMAG\stuestde <stefan.stuetzer@bbraun.com>
2017-08-22 13:07:37 +0200

    /article/gtin Test

---
+++ src/test/java/com/bbraun/hybris/occ/gtin/OCCGTINRequestTest.java
@@ -0,0 +1,54 @@
+package com.bbraun.hybris.occ.gtin;
+
+import org.junit.Test;
+
+import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder;
+import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder.GrantType;
+
+import io.restassured.http.ContentType;
+
+/**
+ * Test of the {@code /article/gtin} OCC webservice endpoint.
+ * 
+ * @author stuestde
+ *
+ */
+public class OCCGTINRequestTest {
+
+	@Test
+	public void testGetArticleSoftaSeptByGtinDeAEMUser() {
+		new BBMWebServiceTestBuilder<>() //
+				.withNewRequest() //
+				.withBasicAuth("trusted_client", "secret") //
+				.doAuthenticateOAuth2("${hybris.oauth.url}", //
+						GrantType.PASSWORD, //
+						"${hybris.oauth.username}", //
+						"${hybris.oauth.password}") //
+				.withNewRequest() //
+				.withQueryParam("applicationKey", "AEM2015")//
+				.withQueryParam("viewId", "de_DE") //
+				.withQueryParam("access_token", "${accessToken}") //
+
+				.doGet("${hybris.occ.gtin.softasept.url}") //
+
+				.assertResponseContentType(ContentType.JSON) //
+				.assertResponseBodyByPathEquals("code", "000000000003887138") //
+				.assertResponseBodyByPathEquals("materialShortText", "SOFTASEPT-N UNCOLORED SPRAY \"DE\" 250ML") //
+		;
+	}
+	
+	@Test
+	public void testGetArticleSoftaSeptByGtinDeAnonymousBarcodeAppUser() {
+		new BBMWebServiceTestBuilder<>() //
+				.withNewRequest() //
+				.withQueryParam("applicationKey", "BarcodeApp")//
+				.withQueryParam("viewId", "de_DE") //
+
+				.doGet("${hybris.occ.gtin.softasept.url}") //
+
+				.assertResponseContentType(ContentType.JSON) //
+				.assertResponseBodyByPathEquals("code", "000000000003887138") //
+				.assertResponseBodyByPathEquals("materialShortText", "SOFTASEPT-N UNCOLORED SPRAY \"DE\" 250ML") //
+		;
+	}
+}
