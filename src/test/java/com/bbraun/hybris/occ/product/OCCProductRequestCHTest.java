e635a4c3ae5ab53e971e957c32d82ffcbcbc2e39
BBMAG\stuestde <stefan.stuetzer@bbraun.com>
2017-08-22 12:30:50 +0200

    /product AEM de_CH Test: product, article

---
+++ src/test/java/com/bbraun/hybris/occ/product/OCCProductRequestCHTest.java
@@ -0,0 +1,63 @@
+package com.bbraun.hybris.occ.product;
+
+import org.junit.Test;
+
+import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder;
+import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder.GrantType;
+
+import io.restassured.http.ContentType;
+
+/**
+ * Tests the /products endpoint with AEM user and "de_CH" view configuration.
+ * 
+ * Test Product: Introcan Safety PRID00001011
+ * Test Article: Introcan Safety 4251611-01
+ * 
+ * @author stuestde
+ */
+public class OCCProductRequestCHTest {
+
+	@Test
+	public void testGetPRIDIntrocanSafetyCHWithAEMUser() {
+		new BBMWebServiceTestBuilder<>() //
+				.withNewRequest() //
+				.withBasicAuth("trusted_client", "secret") //
+				.doAuthenticateOAuth2("${hybris.oauth.url}", //
+						GrantType.PASSWORD, //
+						"${hybris.oauth.username}", //
+						"${hybris.oauth.password}") //
+				.withNewRequest() //
+				.withQueryParam("applicationKey", "AEM2015")//
+				.withQueryParam("viewId", "de_CH") //
+				.withQueryParam("access_token", "${accessToken}") //
+
+				.doGet("${hybris.occ.product.prid1011.url}") //
+
+				.assertResponseContentType(ContentType.JSON) //
+				.assertResponseBodyEqualsReference("${hybris.occ.product.prid1011.expectedFile}",
+						"sapModifiedTime") //
+		;
+	}
+
+	@Test
+	public void testGetArticleIntrocanSafetyCHWithAEMUser() {
+		new BBMWebServiceTestBuilder<>() //
+				.withNewRequest() //
+				.withBasicAuth("trusted_client", "secret") //
+				.doAuthenticateOAuth2("${hybris.oauth.url}", //
+						GrantType.PASSWORD, //
+						"${hybris.oauth.username}", //
+						"${hybris.oauth.password}") //
+				.withNewRequest() //
+				.withQueryParam("applicationKey", "AEM2015")//
+				.withQueryParam("viewId", "de_CH") //
+				.withQueryParam("access_token", "${accessToken}") //
+
+				.doGet("${hybris.occ.product.article425161101.url}") //
+
+				.assertResponseContentType(ContentType.JSON) //
+				.assertResponseBodyEqualsReference("${hybris.occ.product.article425161101.expectedFile}",
+						"sapModifiedTime") //
+		;
+	}
+}
