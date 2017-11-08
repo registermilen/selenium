9c0e8c73da5207da555e10945b25af00357d4189
Stefan Stützer <stefanstuetzer@gmail.com>
2017-11-08 21:04:56 +0100

    add image proxy test for different countries

---
+++ src/test/java/com/bbraun/imageproxy/ImageProxyTest.java
@@ -0,0 +1,58 @@
+package com.bbraun.imageproxy;
+
+import org.junit.Test;
+
+import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder;
+
+/**
+ * Test of the image proxy application
+ * 
+ * @author stefans
+ */
+public class ImageProxyTest {
+
+	@Test
+	public void testGetDefaultImage() {
+		new BBMWebServiceTestBuilder<>() //
+				.withNewRequest() //
+				.doGet("http://images.bbraun.com/article/PRID00000772") //
+				.assertResponseHeaderEquals("Content-Type", "image/jpeg;charset=UTF-8") //
+				.assertResponseHeaderEquals("Content-Length", "12385");
+	}
+
+	@Test
+	public void testGet01Image() {
+		new BBMWebServiceTestBuilder<>() //
+		.withNewRequest() //
+		.doGet("http://images.bbraun.com/01/article/PRID00000772") //
+		.assertResponseHeaderEquals("Content-Type", "image/jpeg;charset=UTF-8") //
+		.assertResponseHeaderEquals("Content-Length", "17067");
+	}
+	
+	@Test
+	public void testGetDEImage() {
+		new BBMWebServiceTestBuilder<>() //
+		.withNewRequest() //
+		.doGet("http://images.bbraun.com/DE/article/PRID00000772") //
+		.assertResponseHeaderEquals("Content-Type", "image/jpeg;charset=UTF-8") //
+		.assertResponseHeaderEquals("Content-Length", "16290");
+	}
+	
+	@Test
+	public void testGetNOImage() {
+		new BBMWebServiceTestBuilder<>() //
+		.withNewRequest() //
+		.doGet("http://images.bbraun.com/no/article/PRID00000772") //
+		.assertResponseHeaderEquals("Content-Type", "image/jpeg;charset=UTF-8") //
+		.assertResponseHeaderEquals("Content-Length", "20822");
+	}
+	
+	@Test
+	public void testGetESImage() {
+		new BBMWebServiceTestBuilder<>() //
+		.withNewRequest() //
+		.doGet("http://images.bbraun.com/es/article/PRID00000772") //
+		.assertResponseHeaderEquals("Content-Type", "image/jpeg;charset=UTF-8") //
+		.assertResponseHeaderEquals("Content-Length", "12586");
+	}
+}
