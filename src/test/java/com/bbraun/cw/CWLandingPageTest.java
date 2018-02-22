2ce3d166ebfc54032f5d02808675de8c1d773799
Stefan Stützer <stefanstuetzer@gmail.com>
2018-02-22 20:11:35 +0100

    fix test

--- src/test/java/com/bbraun/cw/CWLandingPageTest.java
+++ src/test/java/com/bbraun/cw/CWLandingPageTest.java
@@ -1,27 +1,23 @@
 package com.bbraun.cw;
 
-import com.bbraun.bbmtest.ui.UiTest;
-import org.junit.Ignore;
+import com.bbraun.bbmtest.ui.BBMUiTestBuilder;
 import org.junit.Test;
 import org.openqa.selenium.By;
-
-import com.bbraun.bbmtest.ui.BBMUiTestBuilder;
 
 
 public class CWLandingPageTest {
 
-	@Test
-	public void testSearchProductCW() {
-		UiTest.go(builder -> {
-			builder //
-					.doStartBrowser() //
-					.doOpenUrl("https://www.bbraun.de/de.html") //
-					.doType(By.name("q"), "Vasofix")
-					.doSubmitForm(By.className("_search")) //
-					.assertTextDisplayedOnPage("26 Suchergebnisse") //
-			;
-		});
+    @Test
+    public void testSearchProductCW() {
+        new BBMUiTestBuilder<>()
+                .doStartBrowser() //
+                .doOpenUrl("https://www.bbraun.de/de.html") //
+                .doType(By.name("q"), "Vasofix")
+                .doSubmitForm(By.className("_search")) //
+                .assertTextDisplayedOnPage("26 Suchergebnisse") //
+                .doCloseBrowser() //
+        ;
 
-	}
-	
+    }
+
 }
