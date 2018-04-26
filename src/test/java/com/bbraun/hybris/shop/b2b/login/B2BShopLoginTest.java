347d391eb1bfad5ec9205ddc8b3f4a55915b30f4
BBMAG\stuestde <stefan.stuetzer@bbraun.com>
2018-04-26 11:32:12 +0200

    fix test error

--- src/test/java/com/bbraun/hybris/shop/b2b/login/B2BShopLoginTest.java
+++ src/test/java/com/bbraun/hybris/shop/b2b/login/B2BShopLoginTest.java
@@ -11,64 +11,58 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class B2BShopLoginTest {
 
-	@Rule
-	public RunOnStageRule rule = new RunOnStageRule();
+    @Rule
+    public RunOnStageRule rule = new RunOnStageRule();
 
 
-	@Test
-	@RunOnStage(stages = "QAS")
-	public void testLoginIDPBOBUserSuccess() {
+    @Test
+    @RunOnStage(stages = "QAS")
+    public void testLoginIDPBOBUserSuccess() {
         UiTest.go(builder -> {
             builder.doStartBrowser() //
-				.doOpenUrl("https://qas-shop.bbraun.com")
+                    .doOpenUrl("https://qas-shop.bbraun.com")
                     .doWaitUntil(ExpectedConditions.urlContains("idp-dev.bbraun.com"))
-				.assertUrl("https://idp-dev.bbraun.com/idp/SSO.saml2") //
-					.doType(By.id("username"), "shopdemo@bbraun.com") //
-					.doType(By.id("password"), "demo") //
-				.doSubmitForm(By.className("button-primary")) //
-				.assertUrl("https://qas-shop.bbraun.com/bobs") //
+                    .assertUrl("https://idp-dev.bbraun.com/idp/SSO.saml2")
+                    .doType(By.id("username"), "shopdemo@bbraun.com")
+                    .doType(By.id("password"), "demo")
+                    .doSubmitForm(By.className("button-primary"))
+                    .assertUrl("https://qas-shop.bbraun.com/bob")
+            ;
+        });
+    }
+
+    @Test
+    @RunOnStage(stages = "QAS")
+    public void testOpenSavedCarts() {
+        UiTest.go(builder -> {
+            builder.doStartBrowser() //
+                    .doMaximizeWindow() //
+                    .doOpenUrl("https://qas-shop.bbraun.com")
+                    .doWaitUntil(ExpectedConditions.urlContains("idp-dev.bbraun.com"))
+                    .assertUrl("https://idp-dev.bbraun.com/idp/SSO.saml2") //
+                    .doType(By.id("username"), "shopdemo@bbraun.com") //
+                    .doType(By.id("password"), "demo") //
+                    .doSubmitForm(By.className("button-primary")) //
+
+                    .assertUrl("https://qas-shop.bbraun.com/bob") //
+                    .doClick(By.linkText("20005585"))
+                    .doWaitUntil(ExpectedConditions.urlContains("https://qas-shop.bbraun.com"))
+
+                    .doClick(By.linkText("Ihr Konto"))
+                    .assertUrl("https://qas-shop.bbraun.com/my-account/orders")
+
+                    .doClick(By.linkText("Bestellvorlagen"))
+                    .assertUrl("https://qas-shop.bbraun.com/my-account/saved-carts")
+                    .assertTextDisplayedOnPage("Gespeicherte Warenkörbe")
+                    .assertElementPresent(By.linkText("Neue Bestellvorlage anlegen"))
             ;
 
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         });
-	}
-
-	@Test
-	@RunOnStage(stages = "QAS")
-	public void testOpenSavedCarts() {
-		UiTest.go(builder -> {
-			builder.doStartBrowser() //
-					.doMaximizeWindow() //
-					.doOpenUrl("https://qas-shop.bbraun.com")
-					.doWaitUntil(ExpectedConditions.urlContains("idp-dev.bbraun.com"))
-					.assertUrl("https://idp-dev.bbraun.com/idp/SSO.saml2") //
-					.doType(By.id("username"), "shopdemo@bbraun.com") //
-					.doType(By.id("password"), "demo") //
-					.doSubmitForm(By.className("button-primary")) //
-
-					.assertUrl("https://qas-shop.bbraun.com/bob") //
-					.doClick(By.linkText("20005585"))
-					.doWaitUntil(ExpectedConditions.urlContains("https://qas-shop.bbraun.com"))
-
-					.doClick(By.linkText("Ihr Konto"))
-					.assertUrl("https://qas-shop.bbraun.com/my-account/orders")
-
-					.doClick(By.linkText("Bestellvorlagen"))
-					.assertUrl("https://qas-shop.bbraun.com/my-account/saved-carts")
-					.assertTextDisplayedOnPage("Gespeicherte Warenkörbe")
-                    .assertElementPresent(By.linkText("Neue Bestellvorlage anlegen"))
-			;
-
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		});
-	}
+    }
 
 }
