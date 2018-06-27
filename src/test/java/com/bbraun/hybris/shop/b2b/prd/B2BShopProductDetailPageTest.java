d1b066762b4caff917fd5f8e223b4d31faf54b44
grimhnde <hendrik.grimsel@bbraun.com>
2018-06-27 13:25:45 +0200

    New class B2BShopProductDetailPageTest with test for BOM table (QAS & PRD)

---
+++ src/test/java/com/bbraun/hybris/shop/b2b/prd/B2BShopProductDetailPageTest.java
@@ -0,0 +1,37 @@
+package com.bbraun.hybris.shop.b2b.prd;
+
+import com.bbraun.bbmtest.conf.RunOnStage;
+import com.bbraun.bbmtest.conf.RunOnStageRule;
+import com.bbraun.bbmtest.ui.UiTest;
+import com.bbraun.hybris.shop.b2b.B2BActions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+@RunOnStage(stages = "PRD")
+public class B2BShopProductDetailPageTest {
+
+	 @Rule public RunOnStageRule rule = new RunOnStageRule();
+	 
+	 @Test
+	 public void testBOMTable() {
+		 UiTest.go(builder -> {
+	            builder.doStartBrowser() //
+	                    .doMaximizeWindow() //
+	                    .execute(B2BActions::loginPRD) //
+	                    
+	                    .assertUrl("https://shop.bbraun.com/bob") //
+	                    .doClick(By.linkText("20158045"))
+	                    .doWaitUntil(ExpectedConditions.urlContains("https://shop.bbraun.com"))
+	                    .assertTextDisplayedOnPage("20158045")
+	                    
+	                    .doOpenUrl("https://shop.bbraun.com/p/PRID00003923")
+	                    .doWaitUntil(ExpectedConditions.urlContains("https://shop.bbraun.com/p/PRID00003923"))
+	                    .assertTextDisplayedOnPage("Introcan®")
+	                    
+	                    .assertElementExists(By.id("billOfMaterialItemsARTICLES"))
+	            ;
+	        });
+	 }
+}
---
+++ src/test/java/com/bbraun/hybris/shop/b2b/qas/B2BShopProductDetailPageTest.java
@@ -0,0 +1,37 @@
+package com.bbraun.hybris.shop.b2b.qas;
+
+import com.bbraun.bbmtest.conf.RunOnStage;
+import com.bbraun.bbmtest.conf.RunOnStageRule;
+import com.bbraun.bbmtest.ui.UiTest;
+import com.bbraun.hybris.shop.b2b.B2BActions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+@RunOnStage(stages = "QAS")
+public class B2BShopProductDetailPageTest {
+
+	 @Rule public RunOnStageRule rule = new RunOnStageRule();
+	 
+	 @Test
+	 public void testBOMTable() {
+		 UiTest.go(builder -> {
+	            builder.doStartBrowser() //
+	                    .doMaximizeWindow() //
+	                    .execute(B2BActions::loginQAS) //
+	                    
+	                    .assertUrl("https://qas-shop.bbraun.com/bob") //
+	                    .doClick(By.linkText("20005585"))
+	                    .doWaitUntil(ExpectedConditions.urlContains("https://qas-shop.bbraun.com"))
+	                    .assertTextDisplayedOnPage("20005585")
+	                    
+	                    .doOpenUrl("https://qas-shop.bbraun.com/p/PRID00003923")
+	                    .doWaitUntil(ExpectedConditions.urlContains("https://qas-shop.bbraun.com/p/PRID00003923"))
+	                    .assertTextDisplayedOnPage("Introcan®")
+	                    
+	                    .assertElementExists(By.id("billOfMaterialItemsARTICLES"))
+	            ;
+	        });
+	 }
+}
