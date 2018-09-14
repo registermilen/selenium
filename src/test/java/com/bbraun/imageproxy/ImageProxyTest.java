118e884e1a9b84ee0cb5b9717507e2628fbb0d57
stuestde <stefan.stuetzer@bbraun.com>
2018-09-14 08:07:51 +0200

    Use other product image for image proxy test

--- src/test/java/com/bbraun/imageproxy/ImageProxyTest.java
+++ src/test/java/com/bbraun/imageproxy/ImageProxyTest.java
@@ -28,60 +28,36 @@ public class ImageProxyTest {
 	@Test
 	@Severity(SeverityLevel.CRITICAL)
 	@Description("Test Description: Get default image")
-	@Story("Request the default image via Image Proxy for product PRID00000772")
+	@Story("Request the default image via Image Proxy for product PRID00010216")
 	public void testGetDefaultImage() {
 		new BBMWebServiceTestBuilder<>() //
 				.withNewRequest() //
-				.doGet("http://images.bbraun.com/article/PRID00000772") //
+				.doGet("http://images.bbraun.com/article/PRID00010216") //
 				.assertResponseContentType("image/jpeg;charset=UTF-8") //
-				.assertResponseHeaderEquals("Content-Length", "17067");
+				.assertResponseHeaderEquals("Content-Length", "16439");
 	}
 
 	@Test
 	@Severity(SeverityLevel.CRITICAL)
 	@Description("Test Description: Get global image")
-	@Story("Request the global image via Image Proxy for product PRID00000772")
+	@Story("Request the global image via Image Proxy for product PRID00010216")
 	public void testGet01Image() {
 		new BBMWebServiceTestBuilder<>() //
 				.withNewRequest() //
-				.doGet("http://images.bbraun.com/01/article/PRID00000772") //
+				.doGet("http://images.bbraun.com/01/article/PRID00010216") //
 				.assertResponseContentType("image/jpeg;charset=UTF-8") //
-				.assertResponseHeaderEquals("Content-Length", "17067");
+				.assertResponseHeaderEquals("Content-Length", "16439");
 	}
 
 	@Test
 	@Severity(SeverityLevel.CRITICAL)
 	@Description("Test Description: Get product image for Germany")
-	@Story("Request the german image via Image Proxy for product PRID00000772")
+	@Story("Request the german image via Image Proxy for product PRID00010216")
 	public void testGetDEImage() {
 		new BBMWebServiceTestBuilder<>() //
 				.withNewRequest() //
-				.doGet("http://images.bbraun.com/DE/article/PRID00000772") //
+				.doGet("http://images.bbraun.com/DE/article/PRID00010216") //
 				.assertResponseContentType("image/jpeg;charset=UTF-8") //
-				.assertResponseHeaderEquals("Content-Length", "16290");
-	}
-
-	@Test
-	@Severity(SeverityLevel.CRITICAL)
-	@Description("Test Description: Get product image for Norway")
-	@Story("Request the norwegian image via Image Proxy for product PRID00000772")
-	public void testGetNOImage() {
-		new BBMWebServiceTestBuilder<>() //
-				.withNewRequest() //
-				.doGet("http://images.bbraun.com/no/article/PRID00000772") //
-				.assertResponseContentType("image/jpeg;charset=UTF-8") //
-				.assertResponseHeaderEquals("Content-Length", "20822");
-	}
-
-	@Test
-	@Severity(SeverityLevel.CRITICAL)
-	@Description("Test Description: Get product image for Spain")
-	@Story("Request the spain image via Image Proxy for product PRID00000772")
-	public void testGetESImage() {
-		new BBMWebServiceTestBuilder<>() //
-				.withNewRequest() //
-				.doGet("http://images.bbraun.com/es/article/PRID00000772") //
-				.assertResponseContentType("image/jpeg;charset=UTF-8") //
-				.assertResponseHeaderEquals("Content-Length", "13298");
+				.assertResponseHeaderEquals("Content-Length", "16439");
 	}
 }
