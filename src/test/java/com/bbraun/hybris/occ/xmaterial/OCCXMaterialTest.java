cfc66d4149bb36031ce54a9424946c96d1694ba2
BBMAG\manomibg <milen.manov@bbraun.com>
2018-11-06 14:32:09 +0200

    Text for xMaterial occ

---
+++ src/test/java/com/bbraun/hybris/occ/xmaterial/OCCXMaterialTest.java
@@ -0,0 +1,82 @@
+package com.bbraun.hybris.occ.xmaterial;
+
+
+import org.apache.http.HttpStatus;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.bbraun.bbmtest.conf.RunOnStage;
+import com.bbraun.bbmtest.conf.RunOnStageRule;
+import com.bbraun.bbmtest.conf.TestProperty;
+import com.bbraun.bbmtest.conf.TestPropertyRule;
+import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+/**
+ * Tests the /competitormaterial endpoint.
+ *
+ */
+@Epic("OCC Webservice Tests")
+@Feature("OCC XMaterial Endpoint Tests")
+@RunOnStage(stages = {"LOCAL", "QAS"})
+public class OCCXMaterialTest {
+
+    @Rule
+    public RunOnStageRule rule = new RunOnStageRule();
+
+    @ClassRule
+    public static TestPropertyRule testPropertiesRule = new TestPropertyRule();
+
+    @TestProperty("hybris.luser.oauth.username")
+    private static String oAuthUsername;
+
+    @TestProperty("hybris.luser.oauth.password")
+    private static String oAuthPassword;
+
+    @TestProperty("hybris.oauth.url")
+    private static String oAuthUrl;
+
+    @TestProperty("hybris.occ.xmaterial.url")
+    private static String xMaterialUrl;
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description: Data for a competitor material.")
+    @Story("Retrieve data for a competiror material with SAM user.")
+    public void testTranscodeSingleCompetitorMaterialWithSGDAUser() {
+    	RestAssured.useRelaxedHTTPSValidation();
+        new BBMWebServiceTestBuilder<>() //
+                .withNewRequest() //
+                .withBasicAuth("trusted_client", "secret") //
+                .doAuthenticateOAuth2(oAuthUrl, //
+                        BBMWebServiceTestBuilder.GrantType.PASSWORD, //
+                        oAuthUsername, //
+                        oAuthPassword) //
+                .withNewRequest() //
+                .withQueryParam("applicationKey", "SAM")//
+                .withQueryParam("materialIds", "nonexisting,34-702-13-07")//
+                .withQueryParam("access_token", "${accessToken}") //
+
+                .doGet(xMaterialUrl) //
+
+                .assertResponseHttpStatusCode(HttpStatus.SC_OK) //
+                .assertResponseContentType(ContentType.JSON) //
+                .assertResponseBodyByPathEquals("materials.size() == 1", Boolean.TRUE)  //
+                .assertResponseBodyByPathEquals("materials[0].materialNumber", "34-702-13-07") //
+                .assertResponseBodyByPathEquals("materials[0].competitorName", "KLS Martin") //
+                .assertResponseBodyByPathEquals("materials[0].materialsTexts.size() == 1", Boolean.TRUE) //
+                .assertResponseBodyByPathEquals("materials[0].materialsTexts[0].isoCode", "DE") //
+                .assertResponseBodyByPathEquals("materials[0].materialsTexts[0].text", "Fl XI ER HÄKCHEN, GUTHRIE, SCHARF, 13 CM") //
+        ;
+    }
+
+}
