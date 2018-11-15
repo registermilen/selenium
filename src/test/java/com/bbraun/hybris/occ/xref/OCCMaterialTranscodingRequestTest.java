d2593c65ef795e8a438f6e2bf72568b56863f066
BBMAG\penkanbg <angel.penkov@bbraun.com>
2018-11-15 14:02:55 +0200

    Fix OCCMaterialTranscodingRequestTest - use new material

--- src/test/java/com/bbraun/hybris/occ/xref/OCCMaterialTranscodingRequestTest.java
+++ src/test/java/com/bbraun/hybris/occ/xref/OCCMaterialTranscodingRequestTest.java
@@ -1,18 +1,24 @@
 package com.bbraun.hybris.occ.xref;
 
 
+import org.apache.http.HttpStatus;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
 import com.bbraun.bbmtest.conf.RunOnStage;
 import com.bbraun.bbmtest.conf.RunOnStageRule;
 import com.bbraun.bbmtest.conf.TestProperty;
 import com.bbraun.bbmtest.conf.TestPropertyRule;
 import com.bbraun.bbmtest.ws.BBMWebServiceTestBuilder;
-import io.qameta.allure.*;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.restassured.http.ContentType;
-import org.apache.http.HttpStatus;
-import org.junit.ClassRule;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
 
 /**
  * Tests the /materialtranscoding endpoint.
@@ -59,19 +65,19 @@ public class OCCMaterialTranscodingRequestTest {
                 .withQueryParam("fields", "FULL")//
                 .withQueryParam("viewId", "de_DE")//
                 .withQueryParam("access_token", "${accessToken}") //
-                .withQueryParam("materialIds", "34-702-13-07") //
+                .withQueryParam("materialIds", "160-775") //
 
                 .doGet(materialTranscodingWsUrl) //
 
                 .assertResponseHttpStatusCode(HttpStatus.SC_OK) //
                 .assertResponseContentType(ContentType.JSON) //
-                .assertResponseBodyByPathEquals("materials.size() == 1", Boolean.TRUE)  //
-                .assertResponseBodyByPathEquals("materials[0].materialNumber", "34-702-13-07") //
-                .assertResponseBodyByPathEquals("materials[0].vendorId", "9800000835") //
-                .assertResponseBodyByPathEquals("materials[0].vendorName", "KLS Martin") //
+                .assertResponseBodyByPathEquals("materials.size() == 3", Boolean.TRUE)  //
+                .assertResponseBodyByPathEquals("materials[0].materialNumber", "160-775") //
+                .assertResponseBodyByPathEquals("materials[0].vendorId", "9800001183") //
+                .assertResponseBodyByPathEquals("materials[0].vendorName", "George Tiemann & Co") //
                 .assertResponseBodyByPathEquals("materials[0].references.size() > 0", Boolean.TRUE) //
                 .assertResponseBodyByPathEquals("materials[0].references[0].transcodedMaterials.size() > 0", Boolean.TRUE) //
-                .assertResponseBodyByPathEquals("materials[0].references[0].transcodedMaterials[0].code", "OA335R") //
+                .assertResponseBodyByPathEquals("materials[0].references[0].transcodedMaterials[0].code", "BH442R") //
                 .assertResponseBodyByPathEquals("materials[0].references[0].transcodedMaterials[0].materialsTexts.size() > 0", Boolean.TRUE) //
                 .assertResponseBodyByPathNotNull("materials[0].references[0].transcodedMaterials[0].materialsTexts.salesClass") //
                 .assertResponseBodyByPathNotNull("materials[0].references[0].transcodedMaterials[0].materialsTexts.salesClassCode") //
